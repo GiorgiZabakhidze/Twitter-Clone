b00697315c17e16cdbb17d5265526bf85c34ef45
GiorgiZabakhidze <zabaxidzegivi@gmail.com>
2023-08-13 20:25:14 +0400

    SlideMenu .background Fix
    Background Opacity -> 0.5 Fix.

--- Twitter-Clone/Twitter-Clone/View/Main/MainView.swift
+++ Twitter-Clone/Twitter-Clone/View/Main/MainView.swift
@@ -23,7 +23,7 @@ struct MainView: View {
                     SlideMenu()
                         .shadow(color: Color.black.opacity(x != 0 ? 0.1 : 0), radius: 5, x: 5, y: 0)
                         .offset(x: x)
-                        .background(Color.black.opacity(x == 0 ? 0.5 : 0))
+                        .background(Color.black.opacity(x != -width ? 0.5 : 0))
                         .ignoresSafeArea(.all, edges: .vertical)
                         .onTapGesture {
                             withAnimation {
