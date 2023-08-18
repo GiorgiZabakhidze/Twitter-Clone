5327acf7fd878adef02c081ce3d5afab1765d8ca
GiorgiZabakhidze <zabaxidzegivi@gmail.com>
2023-08-18 14:50:29 +0400

    Hashing Password Before Saving It in the Database

--- twitter-api/src/index.js
+++ twitter-api/src/index.js
@@ -11,5 +11,5 @@ app.use(express.json())
 app.use(userRouter)
 
 app.listen(port, () => {
-    console.log("The Server Is Up On Port" + port)
+    console.log("The Server Is Up On Port " + port)
 })
