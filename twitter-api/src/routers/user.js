e56b2c2bf0cbdd864edd52f1a8f546e18607f09b
GiorgiZabakhidze <zabaxidzegivi@gmail.com>
2023-08-18 22:03:02 +0400

    Fetch a Single User

--- twitter-api/src/routers/user.js
+++ twitter-api/src/routers/user.js
@@ -10,7 +10,9 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
+
         res.status(201).send(user)
+
     }catch (err) {
         res.status(400).send(err)
     }
@@ -20,7 +22,9 @@ router.post('/users', async (req, res) => {
 router.get('/users', async (req, res) => {
     try {
         const users = await User.find({})
+
         res.send(users)
+        
     }catch (err) {
         res.status(500).send(err)
     }
@@ -51,6 +55,23 @@ router.delete('/users/:id', async (req, res) => {
 
         res.send()
 
+    }catch (err) {
+        res.status(500).send(err)
+    }
+})
+
+//Fetch a single User
+router.get('/users/:id', async (req, res) => {
+    try {
+        const _id = req.params.id
+
+        const user = await User.findById(_id)
+
+        if(!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
     }catch (err) {
         res.status(500).send(err)
     }
