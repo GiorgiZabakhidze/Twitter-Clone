c51940a5e090718945ae6d5adbd6be0b648c927d
GiorgiZabakhidze <zabaxidzegivi@gmail.com>
2023-08-22 22:49:00 +0400

    Fetch Notifications For all/SingleUser

--- twitter-api/src/routers/notification.js
+++ twitter-api/src/routers/notification.js
@@ -4,6 +4,7 @@ const Notification = require('../models/notification')
 
 const router = new express.Router()
 
+//Create Notification
 router.post('/notification', auth, async (req, res) => {
     const notification = new Notification({
         ...req.body,
@@ -14,8 +15,32 @@ router.post('/notification', auth, async (req, res) => {
         await notification.save()
 
         res.status(201).send(notification)
-        
+
     }catch (err) {
         res.status(400).send(err)
     }
 })
+
+//Fetch Notifications
+router.get('/notification', async (req, res) => {
+    try {
+        const notifications = await Notification.find({})
+        
+        res.send(notifications)
+    }catch (err) {
+        res.status(500).send(err)
+    }
+})
+
+//Fetch a Single User's Notification
+router.get('/notification/:id', async (req, res) => {
+    try {
+        const notifications = await Notification.find({ notReceiverId: req.params.id})
+        
+        res.send(notifications)
+    }catch (err) {
+        res.status(500).send(err)
+    }
+})
+
+module.exports = router
